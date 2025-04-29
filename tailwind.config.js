30bd4ca9d20244d5d36c16bee8039b670ce1ed17
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-04-29 20:31:19 +0200

    add colors and fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}", "!/node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#060404",
+        light: "#fefbfb",
+        primary: "#f45252",
+        secondary: "#f2afaf",
+      },
+      fontFamily: {
+        ledger: ["Ledger", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
