417508e8cc8e7cdde29bc24fdf7624e6d6ef7740
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-05-14 15:29:15 +0200

    edit and delete listing funcitons

---
+++ js/api/listings/deleteListing.js
@@ -0,0 +1,22 @@
+export async function deleteListing(listingId) {
+  const token = getToken();
+
+  if (!token) {
+    throw new Error("Log in to delete listing");
+  }
+
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "X-NOROFF-API-KEY": NOROFF_API_KEY,
+    },
+  };
+  const LISTING_URL = `${ALL_POSTS_URL}/${listingId}`;
+  const response = await fetch(LISTING_URL, options);
+  if (!response.ok) {
+    throw new Error("Failed to delete listing");
+  }
+  return response;
+}
---
+++ js/api/listings/editListing.js
@@ -0,0 +1,29 @@
+import { ALL_POSTS_URL, NOROFF_API_KEY } from "../../constants/api.js";
+import { getToken } from "../../utils/localStorage.js";
+
+export async function editListing(listingId, updatedListing) {
+  const token = getToken();
+
+  if (!token) {
+    throw new Error("Log in to edit listing");
+  }
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "X-NOROFF-API-KEY": NOROFF_API_KEY,
+    },
+    body: JSON.stringify(updatedListing),
+  };
+
+  const LISTING_URL = `${ALL_POSTS_URL}/${listingId}`;
+  const response = await fetch(LISTING_URL, options);
+
+  if (!response.ok) {
+    throw new Error("Failed to update listing");
+  }
+  const json = await response.json();
+  return json.data;
+}
